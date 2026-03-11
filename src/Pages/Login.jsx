46b185b4ae12f8d7d39f55abf782155c975805de
Sachin Kushwaha <sachinkkushwaha991@gmail.comssh-keygen -t ed25519 -C sachinkkushwaha991@gmail.com>
2026-03-11 23:43:34 +0530

    fix folder casing for linux deployment

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import useAuthStore from "../Store/authStore"
+import useAuthStore from "../store/authStore"
 
 function Login() {
 
