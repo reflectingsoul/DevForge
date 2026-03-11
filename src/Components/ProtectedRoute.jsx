23f55b37b4c3848b04c75759bccb9b9c3e61476a
Sachin Kushwaha <sachinkkushwaha991@gmail.comssh-keygen -t ed25519 -C sachinkkushwaha991@gmail.com>
2026-03-11 23:38:14 +0530

    fix folder casing for linux deployment

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom"
-import useAuthStore from "../Store/authStore"
+import useAuthStore from "../store/authStore"
 
 function ProtectedRoute({ children }) {
 
